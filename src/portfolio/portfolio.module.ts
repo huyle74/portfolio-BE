a3f6aa9e5fe01f93a13ecc2bb2d651696b3b2178
huyle74 <legiahuy74@gmail.com>
2024-12-11 19:20:52 +0700

    1

--- src/portfolio/portfolio.module.ts
+++ src/portfolio/portfolio.module.ts
@@ -23,7 +23,7 @@ console.log(
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.DB_HOST || 'localhost',
+      host: process.env.DB_HOST || '127.0.0.1',
       port: 3306,
       username: process.env.DB_USERNAME || 'root',
       password: process.env.DB_PASSWORD || '123456',
